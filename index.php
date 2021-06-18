b22f11c1e9a05031b2aa63c8b072a0388b71c64e
SafaanHashmi <55549354+SafaanHashmi@users.noreply.github.com>
2021-06-18 17:51:34 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Commerce Solutions Indore</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="crossorigin"/>
+    <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;700&amp;display=swap"/>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;700&amp;display=swap" media="print" onload="this.media='all'"/>
+    <noscript>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;700&amp;display=swap"/>
+    </noscript>
+    <link href="css/bootstrap.min.css?ver=1.2.0" rel="stylesheet">
+    <link href="css/font-awesome/css/all.min.css?ver=1.2.0" rel="stylesheet">
+    <link href="css/main.css?ver=1.2.0" rel="stylesheet">
+	<link href="img/title-icon-CS.jpg" rel="icon">
+	<link href="img/apple-touch-icon.jpg" rel="apple-touch-icon">
+  </head>
+  <body id="top"><div class="site-wrapper">
+  <div class="site-wrapper-inner">
+    <div class="cover-container">
+      <div class="masthead clearfix">
+        <div class="inner" style="margin-top: 30px;">
+          <h3 class="masthead-brand">Commerce Solutions</h3>
+          <nav class="nav nav-masthead">
+            
+            <a class="nav-link nav-social" href="https://www.youtube.com/channel/UCPI5qZGvXHnreMf4GQDZ2Jw" title="Youtube"><i class="fab fa-youtube" aria-hidden="true"></i></a>
+			<a class="nav-link nav-social" href="https://www.facebook.com/commercesolutions/" title="Facebook"><i class="fab fa-facebook-f" aria-hidden="true"></i></a>
+           <!-- <a class="nav-link nav-social" href="#" title="Instagram"><i class="fab fa-instagram" aria-hidden="true"></i></a>		
+            <a class="nav-link nav-social" href="#" title="Twitter"><i class="fab fa-twitter" aria-hidden="true"></i></a> -->
+          </nav>
+        </div>
+      </div>      
+      <div class="inner cover">
+        <h1 class="cover-heading">We have got a new address</h1>
+        <p class="lead cover-copy">Visit us on <br> <span style="color:#edcf05">www.commercesolutions.in </span></p>
+        <p class="lead"><a href="http://www.commercesolutions.in" class="btn btn-lg btn-default btn-notify">Visit Now</a></p>
+      </div>
+      <div class="mastfoot">
+        <div class="inner">
+          <p>Thanks for visiting</p>
+        </div>
+      </div>
+      <!--<div class="modal fade" id="subscribeModal" tabindex="-1" role="dialog" aria-labelledby="subscribeModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="subscribeModalLabel">Get Notified on Launch:</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <form>
+                <div class="form-group">
+                  <label for="recipient-name" class="form-control-label">Enter you e-mail to get notified when we launch</label>
+                  <input type="text" class="form-control" id="recipient-name" placeholder="your-name@example.com">
+                </div>
+              </form>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-default">Subscribe</button>
+            </div>
+          </div>
+        </div>
+    </div> -->
+  </div>
+</div>
+    <script src="scripts/jquery.slim.min.js?ver=1.2.0"></script>
+    <script src="scripts/bootstrap.bundle.min.js?ver=1.2.0"></script>
+    <script src="scripts/main.js?ver=1.2.0"></script>
+  </body>
+</html>
